75046da6d1a22608ca7e7fa419a27fab448d4673
amamov <ysangsuk78@gmail.com>
2021-05-16 20:42:33 +0900

    fix tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,7 @@
-// hello world
-const hello: string = "hello typescript";
+import * as dotenv from "dotenv";
+dotenv.config({ path: `${__dirname}/../.env` });
+
+const hello: string = "hello";
+const world: string = process.env.hello || "world";
+
+console.log(hello, world);
