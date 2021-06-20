a08ce17143579be76229ff8b6ec9789e2e1d4084
amamov <ysangsuk78@gmail.com>
2021-06-20 22:15:53 +0900

    [update]

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,4 @@ dotenv.config({ path: `${__dirname}/../.env` });
 const hello: string = 'hello';
 const world: string = process.env.hello || 'world';
 
-// const thisisneverthat = asd
-
 console.log(hello, world);
